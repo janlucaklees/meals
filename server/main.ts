b85749f717af1baa45aa703b16ab0e82e5284150
Jan-Luca Klees <email@janlucaklees.de>
2025-07-05 13:26:09 +0200

    Added more test data

--- server/main.ts
+++ server/main.ts
@@ -4,7 +4,63 @@ import { ScheduleCollection } from "/imports/api/Schedules";
 
 Meteor.startup(async () => {
   if ((await RecipeCollection.find().countAsync()) === 0) {
-    await RecipeCollection.insertAsync({ name: "test" });
+    const dummyMeals = [
+      "Spaghetti Bolognese",
+      "Chicken Curry",
+      "Beef Stroganoff",
+      "Vegetable Stir Fry",
+      "Grilled Cheese Sandwich",
+      "Tomato Soup",
+      "Caesar Salad",
+      "Fried Rice",
+      "Tuna Pasta Bake",
+      "Pancakes",
+      "Omelette",
+      "Lasagna",
+      "Ratatouille",
+      "Sushi Rolls",
+      "Teriyaki Chicken",
+      "Gnocchi with Pesto",
+      "Currywurst",
+      "Falafel Wrap",
+      "Miso Soup",
+      "Baked Salmon",
+      "Chili con Carne",
+      "Quiche Lorraine",
+      "Stuffed Bell Peppers",
+      "Macaroni and Cheese",
+      "Sweet and Sour Chicken",
+      "Bibimbap",
+      "Lentil Dahl",
+      "Pad Thai",
+      "Shepherd’s Pie",
+      "Greek Gyros",
+      "Shrimp Tacos",
+      "Butternut Squash Soup",
+      "Spinach and Ricotta Cannelloni",
+      "Kimchi Fried Rice",
+      "Chicken Alfredo",
+      "Zucchini Fritters",
+      "Pulled Pork Sandwich",
+      "Couscous Salad",
+      "Shakshuka",
+      "Tandoori Chicken",
+      "Egg Fried Noodles",
+      "Thai Green Curry",
+      "Pierogi with Sour Cream",
+      "Beetroot and Goat Cheese Salad",
+      "Ramen with Soft-Boiled Egg",
+      "Tortilla Española",
+      "Moroccan Tagine",
+      "Bulgur Pilaf",
+      "Cauliflower Tikka Masala",
+      "Swedish Meatballs",
+      "Korean BBQ Beef",
+    ];
+
+    for (const name of dummyMeals) {
+      await RecipeCollection.insertAsync({ name });
+    }
   }
 
   Meteor.publish("recipes", function () {
