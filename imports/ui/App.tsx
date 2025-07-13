4186d9cc2142fc6c504e1703316175a508dbc7ef
Jan-Luca Klees <email@janlucaklees.de>
2025-07-13 13:46:57 +0200

    Enabled week navigation
    In this commit I wired up the navigation through the weeks, and chose
    saturday as the start of the week - just because that is how we plan
    our week.
    
    Now I can plan upcoming weeks and see old ones!

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { addWeeks, previousSaturday, subWeeks } from "date-fns";
 import { RecipeFinder } from "./RecipeFinder";
 import { WeekView } from "./WeekView";
 import { Button } from "./components/Button";
@@ -8,17 +9,27 @@ import { Button } from "./components/Button";
 import "./App.css";
 
 export const App = () => {
+  const [startDate, setStartDtate] = useState(new Date());
   return (
     <main>
       <DndProvider backend={HTML5Backend}>
         <div className="p-6 pb-8 flex items-center gap-4 justify-end">
-          <Button label="<" />
-          <Button label="This Week" />
-          <Button label="Next Week" />
-          <Button label=">" />
+          <Button
+            label="<"
+            onClick={() => setStartDtate(subWeeks(startDate, 1))}
+          />
+          <Button label="This Week" onClick={() => setStartDtate(new Date())} />
+          <Button
+            label="Next Week"
+            onClick={() => setStartDtate(addWeeks(new Date(), 1))}
+          />
+          <Button
+            label=">"
+            onClick={() => setStartDtate(addWeeks(startDate, 1))}
+          />
         </div>
         <WeekView
-          startDate={new Date()}
+          startDate={previousSaturday(startDate)}
           slots={["breakfast", "lunch", "dinner"]}
         />
         <RecipeFinder />
--- imports/ui/components/Button.tsx
+++ imports/ui/components/Button.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 type Props = {
   label: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-export const Button: React.FC<Props> = ({ label }) => {
+export const Button: React.FC<Props> = ({ label, onClick }) => {
   return (
     <button
       type="button"
+      onClick={onClick}
       className="px-4 h-10 cursor-pointer rounded-lg bg-lime-500 border-gray-400 border text-sm flex items-center justify-center shadow-md shadow-gray-400 active:shadow-none active:scale-99 transition"
     >
       {label}
